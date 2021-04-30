bacb93364f8648f4f9e7c3ffe38006f22f14dbb8
yashkokane-TUD <77935215+yashkokane-TUD@users.noreply.github.com>
2021-05-01 00:30:44 +0100

    updates

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class gameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
+    private Vector3 StartPos;
+    public static int score;
+    
+    public TMP_Text scoretxt;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +22,39 @@ public class gameManager : MonoBehaviour
     {
         
     }
+    public void RespawnPlayer()
+        {
+            player.transform.position = StartPos;
+        }
+    
+         public void PauseGame()
+         {
+             Time.timeScale = 0;
+             
+         }
+    
+         public void UnPauseGame()
+         {
+             Time.timeScale = 1;
+         }
+    
+         public void RestartGame()
+         {
+             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+         }
+         
+         public void Quit()
+         {
+             //If we are running in a standalone build of the game
+    #if UNITY_STANDALONE
+             //Quit the application
+             Application.Quit();
+    #endif
+    
+             //If we are running in the editor
+    #if UNITY_EDITOR
+             //Stop playing the scene
+             UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+         }
 }
